cb15a9596712bc15303bd58876fc49ca119015f1
Anuj09x <your-email@example.com>
2026-07-09 10:08:37 +0530

    Revert "Fix Vite config to build all HTML files and enable clean URLs"
    This reverts commit c4c1156ec704ad2ed8e0615d8ae98a0eb4cfd49f.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import fs from 'fs';
-
-// Automatically gather all .html files in the root folder as entry points
-const htmlFiles = fs.readdirSync(__dirname).filter(file => file.endsWith('.html'));
-const inputs = {};
-htmlFiles.forEach(file => {
-  const name = file.replace(/\.html$/, '');
-  inputs[name] = resolve(__dirname, file);
-});
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: inputs
-    }
-  }
+  plugins: [react()]
 });
